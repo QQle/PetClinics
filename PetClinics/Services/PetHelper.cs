d1d7083e8f64974aa63d1de4ac49ccd17de0e8ee
Arslan Kyrbanov <arslik35@gmail.com>
2025-03-16 15:24:47 +0300

    add reminding

--- PetClinics/Services/PetHelper.cs
+++ PetClinics/Services/PetHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PetClinics.Models;
 
 namespace PetClinics.Services
 {
@@ -15,7 +16,8 @@ namespace PetClinics.Services
             var pets = await _context.Pets
                 .Where(p => p.PetsOwners.Any(up => up.UserId == userId.ToString()))
                 .ToListAsync();
-            return pets;
+            var petsWithRevaccinationInfo = await NeedRevaccination(pets);
+            return new { pets = petsWithRevaccinationInfo }; 
         }
 
         public async Task<string> GetPetsByBidId(Guid bidId)
@@ -25,6 +27,42 @@ namespace PetClinics.Services
             .Select(b => b.Pet.Name)
             .FirstOrDefaultAsync();
             return petName;
+        }
+
+        private async Task<List<object>> NeedRevaccination(List<Pet> pets)
+        {
+            var currentDate = DateTime.Now;
+            var vaccinationId = new Guid("CA3AB1E8-9685-4822-81FB-4FC0BA9ED355");
+            var result = new List<object>();
+
+            foreach (var pet in pets)
+            {
+               
+                var vaccinationBids = await _context.Bids
+                    .Where(b => b.PetId == pet.Id && b.Favors.Id == vaccinationId)
+                    .OrderByDescending(b => b.DateOfAdmission)
+                    .ToListAsync();
+
+                bool needRevaccination = false;
+                if (vaccinationBids.Any())
+                {
+                    var lastVaccinationDate = vaccinationBids.First().DateOfAdmission;
+
+                    if ((currentDate - lastVaccinationDate).TotalDays > 365)
+                    {
+                        needRevaccination = true;
+                    }
+                }
+
+                result.Add(new
+                {
+                    Id = pet.Id,
+                    Name = pet.Name,
+                    needRevaccination = needRevaccination
+                });
+            }
+
+            return result;
         }
 
     }
